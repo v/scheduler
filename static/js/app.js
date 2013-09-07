8b4614e504a32c5cc5eff0c7d430ecc53a2ec489
Vaibhav Verma <vaibhav2614@gmail.com>
2013-09-07 19:48:41 -0400

    Pick a campus on the home page

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,45 @@
 $(function() {
-    $('.search-bar').autocomplete({
-        'source': function(request, response) {
-            var term = request.term;
 
-            var results = autocomplete(window.json, term);
-            console.log(results)
-            response(_.map(results, function(item) {
-                if(item.course) {
-                    item.value = window.json['ids'][item.subj]['courses'][item.course]
-                } else {
-                    item.value = window.json['ids'][item.subj]['name'];
-                }
-                return item;
-            }));
-        },
+  function selectCampus(campus) {
+    window.localStorage.setItem('campus', campus);
+    $('.campus').removeClass('disabled');
+    $('button.campus[value="'+campus+'"]').addClass('disabled');
+    $('#scheduler').show();    
 
-        'select': function(event, ui) {
-            if(ui.item.course) {
-                //redirect to course page.
-                window.location.href = '/list?subj='+ui.item.subj+'&course='+ui.item.course;
-            } else {
-                //redirect to department page.
-                window.location.href = '/list?subj='+ui.item.subj;
-            }
-        }
-    });
+  }
+
+  if(window.localStorage.getItem('campus')) {
+    selectCampus(window.localStorage.getItem('campus'));
+  }
+
+  $('.campus').click(function() {
+    selectCampus($(this).val());
+  });
+
+  $('.search-bar').autocomplete({
+    'source': function(request, response) {
+      var term = request.term;
+
+      var results = autocomplete(window.json, term);
+      console.log(results)
+    response(_.map(results, function(item) {
+      if(item.course) {
+        item.value = window.json['ids'][item.subj]['courses'][item.course]
+      } else {
+        item.value = window.json['ids'][item.subj]['name'];
+      }
+      return item;
+    }));
+    },
+
+  'select': function(event, ui) {
+    if(ui.item.course) {
+      //redirect to course page.
+      window.location.href = '/list?subj='+ui.item.subj+'&course='+ui.item.course;
+    } else {
+      //redirect to department page.
+      window.location.href = '/list?subj='+ui.item.subj;
+    }
+  }
+  });
 });
