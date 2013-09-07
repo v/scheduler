5afddf3b7a4e582b57cbcc81e8ad6131e38f212e
Vaibhav Verma <vaibhav2614@gmail.com>
2013-09-07 05:19:08 -0400

    It looks like shit but it works

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests, json
 
 app = Flask(__name__)
@@ -7,6 +7,40 @@ app = Flask(__name__)
 def home():
     data = requests.get('https://rumobile.rutgers.edu/1/indexes/92013_NB_U.json').json()
     return render_template('index.html', json=json.dumps(data))
+
+@app.route('/list')
+def list():
+    if 'course' in request.args:
+        assert 'subj' in request.args
+        data = requests.get('http://sis.rutgers.edu/soc/course.json', params={
+            'campus': 'NB',
+            'semester': '92013',
+            'level': 'U,G', 
+            'subject': request.args['subj'],
+            'courseNumber': request.args['course'],
+        }).json()
+
+        result = [data]
+        subject = request.args['subj']
+        course = request.args['course']
+
+    elif 'subj' in request.args:
+        data = requests.get('http://sis.rutgers.edu/soc/courses.json', params={
+            'campus': 'NB',
+            'semester': '92013',
+            'level': 'U,G', 
+            'subject': request.args['subj'],
+        }).json()
+
+        result = data
+        subject = request.args['subj']
+        course = ""
+    else:
+        return 'You done goofed'
+
+    return render_template('list.html', data=result, 
+            subject=subject,
+            course=course)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,6 +1,28 @@
 $(function() {
-    $('.search-bar').typeahead({
-        'name': 'herp',
-        'local': ['hello', 'world', 'herp', 'werp', 'derp'],
+    $('.search-bar').autocomplete({
+        'source': function(request, response) {
+            var term = request.term;
+
+            var results = autocomplete(window.json, term);
+            response(results)
+            response(_.map(results, function(item) {
+                if(item.course) {
+                    item.value = window.json['ids'][item.subj]['courses'][item.course]
+                } else {
+                    item.value = window.json['ids'][item.subj]['name'];
+                }
+                return item;
+            }));
+        },
+
+        'select': function(event, ui) {
+            if(ui.item.course) {
+                //redirect to course page.
+                window.location.href = '/list?subj='+ui.item.subj+'&course='+ui.item.course;
+            } else {
+                //redirect to department page.
+                window.location.href = '/list?subj='+ui.item.subj;
+            }
+        }
     });
 });
